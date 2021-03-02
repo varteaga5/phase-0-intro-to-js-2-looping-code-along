836ffbb1d4f09838ddb30fea71006025ed355a33
vincent <vincent.arteaga@gmail.com>
2021-03-01 17:45:15 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(nameArr, event) {
+    let newArr = []
+    for (let i = 0; i < nameArr.length; i++) {
+    newArr.push(`Thank you, ${nameArr[i]}, for the wonderful ${event} gift!`);
+    
+  }
+
+  return newArr;
+}
+
+function countDown(num) {
+    let i = 0
+    while (num >= 0){
+        console.log(num--)
+    }
+}
